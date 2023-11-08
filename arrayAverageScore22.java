f7c05075bc2d363fce5d25d9b453affd9aa14161
KazeKagi <safrizalrahman46@gmail.com>
2023-11-08 14:46:02 +0700

    Push AVERAGE

---
+++ arrayAverageScore22.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class arrayAverageScore22 {
+    public static void main(String[] args) {
+        Scanner xc22 = new Scanner(System.in);
+
+        int[] score = new int[10];
+        double total = 0;
+        double average;
+
+        for (int i = 0; i < score.length; i++) {
+            System.out.print("masukan nilai mahasiswa ke-"+i+" : ");
+            score[i] = xc22.nextInt();
+
+            } for (int i = 0; i < score.length; i++) {
+            total+=score[i];
+            }
+            average=total/score.length;
+            System.out.println("rata-rata nilai = "+average);
+            }
+
+    }
+
