02dadb26b7118176f60db82dfb4c96ff169516a3
KazeKagi <safrizalrahman46@gmail.com>
2023-11-08 12:59:51 +0700

    Push Secondly

--- arrayNumbers22.java
+++ arrayNumbers22.java
@@ -5,20 +5,26 @@ public class arrayNumbers22 {
 
     public static void main(String[] args) {
         
-        int [] num22 = new int[5];
+        int bil[] = {5, 17, -9, 11};
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
+        //ArrayNumber22 awal
+        // int [] num22 = new int[5];
 
-            num22[0] = 10;
-            num22[1] = 22;
-            num22[2] = 7;
-            num22[3] = 20;
-            num22[4] = 10;
-        // num22[0] = 10;
-        // num22[0] = 10;
-            System.out.println(num22[0]);
-            System.out.println(num22[1]);
-            System.out.println(num22[2]);
-            System.out.println(num22[3]);
-            System.out.println(num22[4]);
+            
+        
+            // num22[0] = 10;
+            // num22[1] = 22;
+            // num22[2] = 7;
+            // num22[3] = 20;
+            // num22[4] = 10;
+
+            // System.out.println(num22[0]);
+            // System.out.println(num22[1]);
+            // System.out.println(num22[2]);
+            // System.out.println(num22[3]);
+            // System.out.println(num22[4]);
 
     }
 }
